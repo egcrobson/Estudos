f4f51c0f90f2698bbf77f2f2c4f10286a8f0ffbb
egcrobson <35804225+egcrobson@users.noreply.github.com>
2018-01-25 11:19:43 -0200

    primeiro

---
+++ parimapr.py
@@ -0,0 +1,12 @@
+def parimpar(n):
+    while n >= 0:
+        
+            return 'par'
+        if(n == 1):
+            return 'impar'
+        n = n - 2
+    
+print('Descobrir se o numero é par ou impar - sem o uso do resto da divisão')
+num = input("Digite um número: ")
+var = parimpar(int(num))
+print(var)
